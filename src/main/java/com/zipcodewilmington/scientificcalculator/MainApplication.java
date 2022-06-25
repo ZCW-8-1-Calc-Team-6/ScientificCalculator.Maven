57537ee7f26ea1eca3113040500be6a90dcf4f4c
Ryan <ryan@zipcodersmbp55.lan>
2022-06-25 13:46:21 -0400

    my first swing button

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,15 +1,59 @@
 package com.zipcodewilmington.scientificcalculator;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by leon on 2/9/18.
  */
-public class MainApplication {
+public class MainApplication implements ActionListener {
+
+    static JFrame f;
+    static JTextField l;
+    String operandLeft;
+    String operandRight;
+    String operator;
+
+    public void calculator() {
+        operandLeft = "";
+        operandRight = "";
+        operator = "";
+    }
+
     public static void main(String[] args) {
-        // Ryan will work here on GUI and calculator features
+
+        // Ryan will work here on GUI
+        f = new JFrame("Scientific Calculator");
+        l = new JTextField();
+        l.setEditable(false); // limit interaction to button clicks
+
+        MainApplication theCalc = new MainApplication();
+
+        JButton buttonEquals;
+
+        buttonEquals = new JButton("=");
+
+        JPanel p = new JPanel();
+        p.setBackground(Color.black);
+
+        p.add(buttonEquals);
+
+        buttonEquals.addActionListener(theCalc);
+
+        f.add(p);
+        //f.pack();
+        f.setVisible(true);
 
         // this will call methods in two files
         // based on what operator was selected <- switch case will go here
             // basic functions
             // scientific functions
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
