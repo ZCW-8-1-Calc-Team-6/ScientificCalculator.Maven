04f255fb7f1dceba3fe3726ca4276064e0691355
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-06-25 11:03:32 -0400

    created 2 draft test cases for addition

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,4 +1,7 @@
 package com.zipcodewilmington.scientific_calculator;
+import com.zipcodewilmington.scientificcalculator.BasicFunctions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by leon on 2/9/18.
@@ -6,4 +9,14 @@ package com.zipcodewilmington.scientific_calculator;
 public class TestMainApplication {
 
     // all tests here
+    @Test
+    void twoPlusTwoEqualsFour(){
+        test calculator = new BasicFunctions();
+        assertEquals(4, BasicFunctions.add(2, 2));
+    }
+    @Test
+    void fivePlusTwoEqualsSeven(){
+        test calculator = new BasicFunctions();
+        assertEquals(7, BasicFunctions.add(5, 2));
+    }
 }
