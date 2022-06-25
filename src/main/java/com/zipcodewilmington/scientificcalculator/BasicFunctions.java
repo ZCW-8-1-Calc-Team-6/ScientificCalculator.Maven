cd944ac23284bbc0babec680f84feb0b5a0d8b5b
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-06-25 11:38:32 -0400

    Added switch case

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
@@ -1,6 +1,59 @@
 package com.zipcodewilmington.scientificcalculator;
 
-public class BasicFunctions() {
+import java.util.Scanner;
+
+public class BasicFunctions {
     // Tristan will write methods for basic calculator functions
     // e.g. + - / *
-}
+
+    public static void prompt(){System.out.println("Enter a number");}
+    public static int parse(String y) {int b = Integer.parseInt(y);
+        return b;}
+
+                public static void main (String[]args){
+                Scanner scan = new Scanner(System.in);
+
+                System.out.println("Addition, Subtraction, Multiplication, Division?");
+                String input = scan.nextLine();
+
+                    switch (input) {
+                        case "Addition" -> {
+                            prompt();
+                            String num1 = scan.nextLine();
+                            prompt();
+                            String num2 = scan.nextLine();
+                            System.out.println(parse(num1) + parse(num2));
+                            break;
+                        }
+                        case "Subtraction" -> {
+                            prompt();
+                            String num1 = scan.nextLine();
+                            prompt();
+                            String num2 = scan.nextLine();
+                            System.out.println(parse(num1) - parse(num2));
+                            break;
+                        }
+                        case "Multiplication" -> {
+                            prompt();
+                            String num1 = scan.nextLine();
+                            prompt();
+                            String num2 = scan.nextLine();
+                            System.out.println(parse(num1) * parse(num2));
+                            break;
+                        }
+                        case "Division" -> {
+                            prompt();
+                            String num1 = scan.nextLine();
+                            prompt();
+                            String num2 = scan.nextLine();
+                            System.out.println(parse(num1) / parse(num2));
+                            break;
+                        }
+                    }
+
+
+
+            }
+
+
+            }
