4a7231915bbeed1613c24aed4481a437047f8aef
Ryan <ryan@zipcodersmbp55.lan>
2022-06-25 13:59:43 -0400

    made GUI updates

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -11,10 +11,20 @@ public class MainApplication implements ActionListener {
 
     static JFrame f;
     static JTextField l;
+
+    // operands and operators
+    //
+    // the calculator will operate on either one or two input numbers
+    // if the user enters 2 + 3
+    // it will be generalized to operandLeft operator operandRight
+    //
+    // some operators or functions of the calculator only need one operand and
+    //      that will be stored in operandLeft
     String operandLeft;
     String operandRight;
     String operator;
 
+    // constructor for MainApplication
     public void calculator() {
         operandLeft = "";
         operandRight = "";
@@ -35,14 +45,19 @@ public class MainApplication implements ActionListener {
         buttonEquals = new JButton("=");
 
         JPanel p = new JPanel();
-        p.setBackground(Color.black);
 
         p.add(buttonEquals);
 
         buttonEquals.addActionListener(theCalc);
 
+        p.setBackground(new Color(186, 142, 191));
+
+
         f.add(p);
-        //f.pack();
+        //f.pack(); may be used instead of setsize? might want to look into layouts
+        f.setSize(800, 400);
+        f.setLocationRelativeTo(null);
+        f.setResizable(false);
         f.setVisible(true);
 
         // this will call methods in two files
