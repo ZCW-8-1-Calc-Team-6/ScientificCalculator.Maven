a61819ec911704251472831b04f56950edd35e4a
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-06-26 11:58:34 -0400

    New changes to basicFunctions

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
@@ -1,59 +1,39 @@
 package com.zipcodewilmington.scientificcalculator;
 
-import java.util.Scanner;
-
 public class BasicFunctions {
+    public static void main(String[] args) {
+    }
+
+    public static String add(double x, double y) {
+        double value = x + y;
+        return Double.toString(value);
+    }
+
+    public static String multiply(double x, double y) {
+        double value = x * y;
+        return Double.toString(value);
+    }
+
+    public static String divide(double x, double y) {
+        double value = x / y;
+        return Double.toString(value);
+    }
+
+    public static String subtract(double x, double y) {
+        double value = x - y;
+        return Double.toString(value);
+    }
+
+    public static double sqrt(double number) {
+        double x = 0;
+        return Math.sqrt(x);
+    }
+    public static String invertSign(Double value){
+        value = value * -1.0;
+        return Double.toString(value);
+    }
+}
+
+
     // Tristan will write methods for basic calculator functions
     // e.g. + - / *
-
-    public static void prompt(){System.out.println("Enter a number");}
-    public static int parse(String y) {int b = Integer.parseInt(y);
-        return b;}
-
-                public static void main (String[]args){
-                Scanner scan = new Scanner(System.in);
-
-                System.out.println("Addition, Subtraction, Multiplication, Division?");
-                String input = scan.nextLine();
-
-                    switch (input) {
-                        case "Addition" -> {
-                            prompt();
-                            String num1 = scan.nextLine();
-                            prompt();
-                            String num2 = scan.nextLine();
-                            System.out.println(parse(num1) + parse(num2));
-                            break;
-                        }
-                        case "Subtraction" -> {
-                            prompt();
-                            String num1 = scan.nextLine();
-                            prompt();
-                            String num2 = scan.nextLine();
-                            System.out.println(parse(num1) - parse(num2));
-                            break;
-                        }
-                        case "Multiplication" -> {
-                            prompt();
-                            String num1 = scan.nextLine();
-                            prompt();
-                            String num2 = scan.nextLine();
-                            System.out.println(parse(num1) * parse(num2));
-                            break;
-                        }
-                        case "Division" -> {
-                            prompt();
-                            String num1 = scan.nextLine();
-                            prompt();
-                            String num2 = scan.nextLine();
-                            System.out.println(parse(num1) / parse(num2));
-                            break;
-                        }
-                    }
-
-
-
-            }
-
-
-            }
